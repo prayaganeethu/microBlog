273ce7fe2455be49321cfcc1942d43905ff8c511
Neethu Mohandas <prayaganeethu@gmail.com>
2017-05-30 14:34:25 +0530

    uses loops in templates for displaying hardcoded posts from followed users

--- app/views.py
+++ app/views.py
@@ -6,4 +6,6 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Neethu'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [{'author': {'nickname': 'Garima'}, 'body': 'It is an awesome day!'}, {
+        'author': {'nickname': 'Manisha'}, 'body': 'Yaayyy, Whatss uppp!!!'}, {'author': {'nickname': 'Aiswarya'}, 'body': 'But whyyyyy!! :P'}]
+    return render_template('index.html', title='Home', user=user, posts=posts)
