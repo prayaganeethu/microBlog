15da47c611a45c73b21c46e71e204a71cf13d940
Neethu Mohandas <prayaganeethu@gmail.com>
2017-05-30 13:52:50 +0530

    Displays hardcoded username on the browser

--- app/views.py
+++ app/views.py
@@ -4,4 +4,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Neethu'}
+    return '''
+<html>
+	<head>
+		<title>Home Page</title>
+	</head>
+	<body>
+		<h1>Hello, ''' + user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
