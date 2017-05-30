36c4bd8fae72dc285fe066e334ae5fe494a96107
Neethu Mohandas <prayaganeethu@gmail.com>
2017-05-30 15:49:08 +0530

    Created a login page which asks for OpenID. CSRF is enabled.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Neethu'}
-    posts = [{'author': {'nickname': 'Garima'}, 'body': 'It is an awesome day!'}, {
-        'author': {'nickname': 'Manisha'}, 'body': 'Yaayyy, Whatss uppp!!!'}, {'author': {'nickname': 'Aiswarya'}, 'body': 'But whyyyyy!! :P'}]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
